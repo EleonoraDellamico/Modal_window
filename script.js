7eae25fbb3fc9479a379d8bd355635a7e74b0bb1
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-05-07 15:55:37 +0200

    added for loop js for btns and two add.aventlistener

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+	btnsOpenModal[i].addEventListener('click', function() {
+		//console.log('clicked');
+		modal.classList.remove('hidden');
+		overlay.classList.remove('hidden');
+	});
+btnCloseModal.addEventListener('click', function() {
+	modal.classList.add('hidden');
+	overlay.classList.add('hidden');
+});
