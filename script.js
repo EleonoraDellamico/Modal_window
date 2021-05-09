2221ac4cf71378e8b8ed6687fa82d9f23a471e48
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-05-09 12:23:56 +0200

    added keydown event

--- script.js
+++ script.js
@@ -5,13 +5,28 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-	btnsOpenModal[i].addEventListener('click', function() {
-		//console.log('clicked');
-		modal.classList.remove('hidden');
-		overlay.classList.remove('hidden');
-	});
-btnCloseModal.addEventListener('click', function() {
+const openModal = function() {
+	//console.log('clicked');
+	modal.classList.remove('hidden');
+	overlay.classList.remove('hidden');
+};
+
+const closeModal = function() {
 	modal.classList.add('hidden');
 	overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+//escape keyboard are globalk event for this reason we emplemnt addeventlistern in document
+document.addEventListener('keydown', function(e) {
+	//console.log('a key was pressed');
+	//console.log(e.key);
+	if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+		//console.log('Esc was pressed');
+		closeModal();
+	}
 });
